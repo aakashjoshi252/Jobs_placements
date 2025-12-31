3eecc7a12cd77eb286148e49f21f37692e504d29
git config --global user.name Akki <akkijoshi1717@gmail.com>
2025-12-31 12:25:29 +0530

    dflnlf

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './styles/components.css'
+import './styles/animations.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
